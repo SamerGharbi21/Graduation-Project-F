ed9421b4e98eb1783f9e43bf4da450edd131383e
waleed <waled656@hotmail.com>
2024-03-05 19:33:39 +0300

    made ng project

---
+++ GP-F/src/app/pages/login-page/login-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FilledButton } from '../../components/filled-button/filled-button.component';
+@Component({
+  selector: 'app-login-page',
+  standalone: true,
+  imports: [FilledButton],
+  templateUrl: './login-page.component.html',
+  styles: ''
+})
+export class LoginPageComponent {
+
+}
